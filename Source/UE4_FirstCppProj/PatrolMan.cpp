66d077c4ce7d400706fbbbeb2d5d6b5298fe6726
myung geol choi <myunggeol@gmail.com>
2019-03-18 12:58:46 +0900

    first commit

---
+++ Source/UE4_FirstCppProj/PatrolMan.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PatrolMan.h"
+
+
+// Sets default values
+APatrolMan::APatrolMan()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APatrolMan::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APatrolMan::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+	float t = GetGameTimeSinceCreation();
+	FVector p = GetActorLocation();
+	p.Y = sin(t) * 100.f;
+
+	SetActorLocation(p);
+}
+
